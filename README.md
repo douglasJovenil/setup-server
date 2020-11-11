94f678f96b221ecde716beda614f3fcbe22f4380
douglasJovenil <douglascastilhos@yahoo.com.br>
2020-11-10 22:07:45 -0300

    done: docs

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Setup Virtual Machine
 
-wget -O - https://raw.githubusercontent.com/douglasJovenil/setup-virtual-machine/main/src/script.sh | bash
+This script helps to automate the installation of the following applications on machine:
+
+- [Python 3.8](https://www.python.org/downloads)
+- [Virtualenv](https://virtualenv.pypa.io/en/latest)
+- [Docker](https://www.docker.com)
+- [Docker Compose](https://docs.docker.com/compose)
+- [No-IP](https://www.noip.com)
+
+## 💻 Project
+
+#### Running Script
+
+![Running Script](docs/images/00_running_script.png)
+
+#### Script Done
+
+![Script Done](docs/images/01_script_done.png)
+
+#### Checking Softwares
+
+![Checking Softwares](docs/images/02_checking_softwares.png)
+
+## 🚀 Technologies
+
+<img align="left" alt="Docker" width="26px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/terminal/terminal.png" /> Bash
+
+## 🏃 Usage
+
+```
+bash <(curl -s https://raw.githubusercontent.com/douglasJovenil/setup-virtual-machine/main/src/script.sh)
+```
--- src/script.sh
+++ src/script.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Installing Python3.8 and Virtualenv
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install python3.8 virtualenv -y
-echo 'alias py="/usr/bin/python3.8"' >> ~/.bashrc
+# # Installing Python3.8 and Virtualenv
+# sudo add-apt-repository ppa:deadsnakes/ppa -y
+# sudo apt-get update -y
+# sudo apt-get upgrade -y
+# sudo apt-get install python3.8 virtualenv -y
+# echo 'alias py="/usr/bin/python3.8"' >> ~/.bashrc
 
-# Installing Docker
-sudo apt-get remove docker docker-engine docker.io containerd runc -y
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+# # Installing Docker
+# sudo apt-get remove docker docker-engine docker.io containerd runc -y
+# sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+# sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo apt-key fingerprint 0EBFCD88
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# sudo apt-get update -y
+# sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-# Installing docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+# # Installing docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+# sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Installing noip2
 sudo apt-get install build-essential dkms autoconf automake gcc cpp g++ -y
@@ -29,9 +29,9 @@ rm noip-duc-linux.tar.gz
 cd noip-2.1.9-1
 sudo make
 sudo make install
-cd ..
-rm noip-2.1.9-1 -rf
-rm ._noip-2.1.9-1
+# cd ..
+# rm noip-2.1.9-1 -rf
+# rm ._noip-2.1.9-1
 
 # Restart terminal
 exec bash
